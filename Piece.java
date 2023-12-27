6b231aa129f7a0109f7cbf60da85d8d54b5addaf
Diegoo11 <ynoacamino@gmail.com>
2023-12-27 16:05:35 -0500

    add changeColor method

--- Piece.java
+++ Piece.java
@@ -7,13 +7,15 @@ public class Piece extends JButton {
   private int positionX;
   private int positionY;
   private int team;
+  private Color color;
 
   public Piece(int positionX, int positionY, ImageIcon icon, int team) {
     this.positionX = positionX;
     this.positionY = positionY;
     this.team = team;
 
-    // super.setIcon(icon);
+    revalidateColor();
+
     setIcon(icon);
   };
   
@@ -38,10 +40,46 @@ public class Piece extends JButton {
   public void setPositionY(int y) {
     positionY = y;
   };
+
+  public void setPositions(int x, int y) {
+    positionX = x;
+    positionY = y;
+    revalidateColor();
+  };
+
   public int getTeam() {
     return team;
   };
   public void isPosible() {
     setBackground(Color.ORANGE);
+  };
+
+  public void setBlack() {
+    color = Color.BLACK;
+    setBackground(Color.BLACK);
+  };
+
+  public void setWhite() {
+    color = Color.WHITE;
+    setBackground(Color.WHITE);
+  };
+
+  public void select() {
+    setBackground(Color.RED);
+  };
+
+  public void unselect() {
+    setBackground(color);
+  };
+
+  public void revalidateColor() {
+    if (
+      this.positionX % 2 != 0 && this.positionY % 2 != 0
+      || this.positionX % 2 == 0 && this.positionY % 2 == 0
+    ) {
+      setWhite();
+    } else {
+      setBlack();
+    }
   }
 }
