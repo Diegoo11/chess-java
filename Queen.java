07b3a9ac8ee689fdd776c015c82655d8828a5a2b
Diegoo11 <ynoacamino@gmail.com>
2024-01-01 09:35:35 -0500

    add getPosiblePositions to Queen

--- Queen.java
+++ Queen.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 
 public class Queen extends Piece{
@@ -6,5 +8,110 @@ public class Queen extends Piece{
 
   public Queen(int positionX, int positionY, int team) {
     super(positionX, positionY, team == 0 ? whiteIcon : blackIcon, team);
+  };
+
+    public ArrayList<Piece> getPosiblePositions(Piece[][] board) {
+    ArrayList<Piece> positions = new ArrayList<Piece>();
+
+    int x = getPositionX();
+    int y = getPositionY();
+
+    for (int i = x; i >= 0; i -= 1) {
+      Piece position = board[i][y];
+      if(position instanceof VoidCel){
+        positions.add(position);
+      }else if(position.getTeam() != this.getTeam()){
+        positions.add(position);
+        break;
+      }
+    }
+
+    for(int i = x; i < board.length; i += 1){
+      Piece position = board[i][y];
+      if(position instanceof VoidCel){
+        positions.add(position);
+      }else if(position.getTeam() != this.getTeam()){
+        positions.add(position);
+        break;
+      }
+    }
+
+    for(int j = y; j >= 0; j -= 1){
+      Piece position = board[x][j];
+      if(position instanceof VoidCel){
+        positions.add(position);
+      }else if(position.getTeam() != this.getTeam()){
+        positions.add(position);
+        break;
+      }
+    }
+
+    for(int j = y; j < board.length; j += 1){
+      Piece position = board[x][j];
+      if(position instanceof VoidCel){
+        positions.add(position);
+      }else if(position.getTeam() != this.getTeam()){
+        positions.add(position);
+        break;
+      }
+    }
+    
+    for (int i = 1; i < board.length; i += 1) {
+      if (x + i < board.length && y + i < board.length) {
+        Piece position = board[x + i][y + i];
+        if(position instanceof VoidCel){
+          positions.add(position);
+        } else if(position.getTeam() != this.getTeam()){
+          positions.add(position);
+          break;
+        } else if(position.getTeam() == this.getTeam()){
+          break;
+        }
+      }
+    }
+
+    for (int i = 1; i < board.length; i += 1) {
+      if (x - i > -1 && y - i > -1) {
+        Piece position = board[x - i][y - i];
+        if(position instanceof VoidCel){
+          positions.add(position);
+        } else if(position.getTeam() != this.getTeam()){
+          positions.add(position);
+          break;
+        } else if(position.getTeam() == this.getTeam()){
+          break;
+        }
+      }
+    }
+
+    for (int i = 1; i < board.length; i += 1) {
+      if (x + i < board.length && y - i > -1) {
+        Piece position = board[x + i][y - i];
+        if(position instanceof VoidCel){
+          positions.add(position);
+        } else if(position.getTeam() != this.getTeam()){
+          positions.add(position);
+          break;
+        } else if(position.getTeam() == this.getTeam()){
+          break;
+        }
+      }
+    }
+
+    for (int i = 1; i < board.length; i += 1) {
+      if (x - i > -1 && y + i < board.length) {
+        Piece position = board[x - i][y + i];
+        if(position instanceof VoidCel){
+          positions.add(position);
+        } else if(position.getTeam() != this.getTeam()){
+          positions.add(position);
+          break;
+        } else if(position.getTeam() == this.getTeam()){
+          break;
+        }
+      }
+    }
+    
+    return positions;
   };
 }
