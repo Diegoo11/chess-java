c6226ff130a8823a5d4882428f099421658a3755
Diegoo11 <ynoacamino@gmail.com>
2024-01-01 19:03:26 -0500

    fix Peon move

--- Peon.java
+++ Peon.java
@@ -7,31 +7,33 @@ public class Peon extends Piece {
   private static ImageIcon blackIcon = new ImageIcon("./assets/blackPeon.png");
   private boolean firstMove = true;
 
+  private int direction;
+
   public Peon(int positionX, int positionY, int team) {
     super(positionX, positionY, team == 0 ? whiteIcon : blackIcon, team);
+    direction = team == 0 ? 1 : -1;
   };
 
   public ArrayList<Piece> getPosiblePositions(Piece[][] board) {
     ArrayList<Piece> positions = new ArrayList<Piece>();
     for (int i = 1; i <= (firstMove ? 2:1); i += 1) {
-      System.out.println(getPositionX() + " " + getPositionY() + " " + i);
-      if (getPositionX() + i < 0 || getPositionX() + i > 7) {
+      if (getPositionX() + i * direction < 0 || getPositionX() + i * direction > 7) {
         continue;
       }
-      Piece position = board[getPositionX() + i][getPositionY()];
+      Piece position = board[getPositionX() + i * direction][getPositionY()];
       if (position instanceof VoidCel) {
         positions.add(position);
       }
     }
-    if (getPositionX() + 1 >= 0 && getPositionX() + 1 <= 7
+    if (getPositionX() + 1 * direction >= 0 && getPositionX() + 1 * direction <= 7
         && getPositionY() - 1 >= 0 && getPositionY() + 1 <= 7) {
-      Piece position = board[getPositionX() + 1][getPositionY() + 1];
+      Piece position = board[getPositionX() + 1 * direction][getPositionY() + 1];
 
       if (!(position instanceof VoidCel) && position.getTeam() != getTeam()) {
         positions.add(position);
       }
 
-      position = board[getPositionX() + 1][getPositionY() - 1];
+      position = board[getPositionX() + 1 * direction][getPositionY() - 1];
       if (!(position instanceof VoidCel) && position.getTeam() != getTeam()) {
         positions.add(position);
       }
