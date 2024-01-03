5bf0f636fa3750647b7322102b866cd67bd04d54
Diegoo11 <ynoacamino@gmail.com>
2024-01-03 17:57:52 -0500

    implement Turno class

--- Board.java
+++ Board.java
@@ -4,20 +4,92 @@ import java.awt.event.*;
 import java.util.*;
 public class Board extends JFrame {
   private Piece[][] board = new Piece[8][8];
+  private Turno turno = new Turno();
 
   private Piece finishBtn = null;
   private ArrayList<Piece> posiblePositions;
 
   public Board() {
+    newGame();
+  }
+
+  private class ListenerBtn implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      Piece startBtn = (Piece) e.getSource();
+
+      if (finishBtn == null) {
+        if (startBtn instanceof VoidCel) return;
+        if (startBtn.getTeam() != turno.getTurno()) return;
+        startBtn.select();
+        
+        posiblePositions = startBtn.getPosiblePositions(board);
+        paintPositions(posiblePositions);
+
+        finishBtn = startBtn;
+      } else {
+        removeBoard();
+
+        if (posiblePositions.contains(startBtn) && !finishBtn.equals(startBtn)) {
+          changePositions(board, finishBtn, startBtn, posiblePositions);
+          turno.played();
+        }
+
+        reFill();
+
+        finishBtn.defaultColor();
+
+        posiblePositions = null;
+        finishBtn = null;
+
+        revalidate();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    new Board();
+  };
+
+  public void removeBoard() {
+    for (int i = 0; i < 8; i += 1) {
+      for (int j = 0; j < 8; j += 1) {
+        board[i][j].defaultColor();
+        remove(board[i][j]);
+      }
+    }
+  }
+
+  public void reFill() {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) { 
+        add(board[i][j]);
+      }
+    }
+  }
+
+  public void changePositions(Piece[][] board, Piece p1, Piece p2, ArrayList<Piece> posiblePositions) {
+    board[p1.getPositionX()][p1.getPositionY()] = new VoidCel(p1.getPositionX(), p1.getPositionY());
+    board[p1.getPositionX()][p1.getPositionY()].addActionListener(new ListenerBtn());
+    p1.setPositions(p2.getPositionX(), p2.getPositionY());
+    board[p2.getPositionX()][p2.getPositionY()] = p1;
+  }
+
+  public void paintPositions(ArrayList<Piece> positions) {
+    for (Piece position : positions) {
+      position.isPosible();
+    }
+  }
+
+  public void newGame() {
     for (int i = 0; i < 8; i++) {
       if(i == 0) {
         board[i][0] = new Torre(i, 0, 0);
         add(board[i][0]);
 
-        board[i][1] = new Alfil(i, 1, 0);
+        board[i][1] = new Horse(i, 1, 0);
         add(board[i][1]);
 
-        board[i][2] = new Horse(i, 2, 0);
+        board[i][2] = new Alfil(i, 2, 0);
         add(board[i][2]);
 
         board[i][3] = new Queen(i, 3, 0);
@@ -56,10 +128,10 @@ public class Board extends JFrame {
         board[i][0] = new Torre(i, 0, 1);
         add(board[i][0]);
 
-        board[i][1] = new Alfil(i, 1, 1);
+        board[i][1] = new Horse(i, 1, 1);
         add(board[i][1]);
 
-        board[i][2] = new Horse(i, 2, 1);
+        board[i][2] = new Alfil(i, 2, 1);
         add(board[i][2]);
 
         board[i][3] = new Queen(i, 3, 1);
@@ -86,70 +158,5 @@ public class Board extends JFrame {
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setVisible(true);
   };
-
-  private class ListenerBtn implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-      Piece startBtn = (Piece) e.getSource();
-      
-      if (finishBtn == null) {
-        if (startBtn instanceof VoidCel) return;
-        
-        startBtn.select();
-        
-        posiblePositions = startBtn.getPosiblePositions(board);
-        paintPositions(posiblePositions);
-
-        finishBtn = startBtn;
-      } else {
-        removeBoard();
-
-        changePositions(board, finishBtn, startBtn, posiblePositions);
-
-        reFill();
-
-        finishBtn.defaultColor();
-
-        posiblePositions = null;
-        finishBtn = null;
-
-        revalidate();
-      }
-    }
-  }
-
-  public static void main(String[] args) {
-    new Board();
-  };
-
-  public void removeBoard() {
-    for (int i = 0; i < 8; i += 1) {
-      for (int j = 0; j < 8; j += 1) {
-        board[i][j].defaultColor();
-        remove(board[i][j]);
-      }
-    }
-  }
-
-  public void reFill() {
-    for (int i = 0; i < 8; i++) {
-      for (int j = 0; j < 8; j++) { 
-        add(board[i][j]);
-      }
-    }
-  }
-
-  public void changePositions(Piece[][] board, Piece p1, Piece p2, ArrayList<Piece> posiblePositions) {
-    if (posiblePositions.contains(p2)) {
-      board[p1.getPositionX()][p1.getPositionY()] = new VoidCel(p1.getPositionX(), p1.getPositionY());
-      board[p1.getPositionX()][p1.getPositionY()].addActionListener(new ListenerBtn());
-      p1.setPositions(p2.getPositionX(), p2.getPositionY());
-      board[p2.getPositionX()][p2.getPositionY()] = p1;
-    }
-  }
-
-  public void paintPositions(ArrayList<Piece> positions) {
-    for (Piece position : positions) {
-      position.isPosible();
-    }
-  }
+  
 }
